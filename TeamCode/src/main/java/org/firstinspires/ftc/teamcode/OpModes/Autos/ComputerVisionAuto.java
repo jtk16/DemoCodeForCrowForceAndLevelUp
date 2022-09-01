de858c9ddeef8a1f5fd6bb17abc0ef9efb79d439
jtk16 <jack.t.kinney@gmai.com>
2022-08-31 19:39:50 -0700

    yeehaw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autos/ComputerVisionAuto.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+
+/**
+ * IT IS ASSUMED THAT YOU HAVE READ AND UNDERSTOOD THE EXAMPLEAUTO
+ * AUTO BEFORE PROCEEDING.
+ */
+@Autonomous(name="opencv auto")
+public class ComputerVisionAuto extends LinearOpMode {
+    String CAMERA_CONFIG_NAME = "Cam";
+    OpenCvCamera camera;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // I don't completely understand how this line works but I know that it does.
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        // defines the camera by passing through the camera on hardware map into a wrapper class from EasyOpenCV
+        // that will make reading data easier.
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, CAMERA_CONFIG_NAME), cameraMonitorViewId);
+
+        //set the pipeline that the camera will run
+        camera.setPipeline(new ComputerVisionPipeline());
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            // This method of course runs when the camera is opened.
+            @Override
+            public void onOpened() {
+
+                // This is what actually starts running the pipeline, NOT THE onOpened() METHOD ITSELF.
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+    }
+
+    // This is a method that I use to make transferring info from the pipeline to the auto easier.
+    public static class CameraData {
+        public static String infoFromThePipeline = "";
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autos/ComputerVisionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autos/ComputerVisionPipeline.java
@@ -1,12 +1,89 @@
 package org.firstinspires.ftc.teamcode.OpModes.Autos;
 
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-public class ComputerVisionPipeline extends OpenCvPipeline {
+
+/**
+ * THIS CLASS IS NOT AN AUTONOMOUS.
+ * IN ORDER TO RUN OPENCV IN AUTO OR TELEOP YOU NEED TO RUN THIS PIPELINE
+ * IN TANDEM WITH THE OPMODE BEING RUN.
+ */
+public class ComputerVisionPipeline extends OpenCvPipeline
+{
+    boolean viewportPaused;
+
+    /*
+     * NOTE: if you wish to use additional Mat objects in your processing pipeline, it is
+     * highly recommended to declare them here as instance variables and re-use them for
+     * each invocation of processFrame(), rather than declaring them as new local variables
+     * each time through processFrame(). This removes the danger of causing a memory leak
+     * by forgetting to call mat.release(), and it also reduces memory pressure by not
+     * constantly allocating and freeing large chunks of memory.
+     */
 
     @Override
-    public Mat processFrame(Mat input) {
-        return null;
+    public Mat processFrame(Mat input)
+    {
+        /*
+         * IMPORTANT NOTE: the input Mat that is passed in as a parameter to this method
+         * will only dereference to the same image for the duration of this particular
+         * invocation of this method. That is, if for some reason you'd like to save a copy
+         * of this particular frame for later use, you will need to either clone it or copy
+         * it to another Mat.
+         */
+
+        /*
+         * Draw a simple box around the middle 1/2 of the entire frame
+         */
+        Imgproc.rectangle(
+                input,
+                new Point(
+                        input.cols()/4f,
+                        input.rows()/4f),
+                new Point(
+                        input.cols()*(3f/4f),
+                        input.rows()*(3f/4f)),
+                new Scalar(0, 255, 0), 4);
+
+        ComputerVisionAuto.CameraData.infoFromThePipeline = "this is one way to take data from the vision pipeline to your auto";
+
+        /**
+         * NOTE: to see how to get data from your pipeline to your OpMode as well as how
+         * to change which stage of the pipeline is rendered to the viewport when it is
+         * tapped.
+         */
+
+        return input;
+    }
+
+    @Override
+    public void onViewportTapped()
+    {
+        /*
+         * The viewport (if one was specified in the constructor) can also be dynamically "paused"
+         * and "resumed". The primary use case of this is to reduce CPU, memory, and power load
+         * when you need your vision pipeline running, but do not require a live preview on the
+         * robot controller screen. For instance, this could be useful if you wish to see the live
+         * camera preview as you are initializing your robot, but you no longer require the live
+         * preview after you have finished your initialization process; pausing the viewport does
+         * not stop running your pipeline.
+         *
+         * Here we demonstrate dynamically pausing/resuming the viewport when the user taps it
+         */
+
+        /*viewportPaused = !viewportPaused;
+
+        if(viewportPaused)
+        {
+            webcam.pauseViewport();
+        }
+        else
+        {
+            webcam.resumeViewport();
+        }*/
     }
 }
